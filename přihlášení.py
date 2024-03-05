2cd82e9f2e0cb7507d93cb0b670de55073185d48
MaestroMartin <161080106+MaestroMartin@users.noreply.github.com>
2024-03-05 20:16:39 +0100

    Add files via upload

---
+++ přihlášení.py
@@ -0,0 +1,20 @@
+
+ID = {"martin" : 1234568, "adam" :1234  }
+
+
+vstup1 = input()
+def ID(): 
+    if ID == True :
+        print("zadejte heslo: ") # type: ignore
+    elif False:
+        	print("zadane spatne jmeno ")
+
+
+vstup2 = input(int("heslo: "))
+def ID():
+    if False:
+        print("zadali ste spatne heslo")
+
+if  vstup1 == True and vstup2 == True:
+        print("prihlasovani ")
+
