5339207a354b69692b1c43fb047d330517dc258d
MaestroMartin <161080106+MaestroMartin@users.noreply.github.com>
2024-03-10 19:39:46 +0100

    Update přihlášení.py

--- přihlášení.py
+++ přihlášení.py
@@ -4,15 +4,16 @@ ID = {"martin" : 1234568, "adam" :1234  }
 
 vstup1 = input("jmeno: ")
 if jmeno in ID
-    elif jmeno == True :
+elif jmeno == True :
         print("zadejte heslo: ") # type: ignore
 elif jmeno == False:
         	print("zadane spatne jmeno ")
 
 
 vstup2 = input(int("heslo: "))
-if value in jmeno
-    elif ID == False:
+if value in jmeno:
+    (heslo) == ([])
+elif ID == False:
         print("zadali ste spatne heslo")
 
 if  vstup1 == True and vstup2 == True:
